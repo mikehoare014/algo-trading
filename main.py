55c72ad104a82017497b9b972487748feaeeffc4
Big Mikey <mikeynews014@gmail.com>
2025-10-19 21:06:54 -0500

    chagnes

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ stock_data = yf.download(ticker_symbol, start=start_date, end=end_date)
 stock_data['sma_50'] = stock_data['Close'].rolling(window=50).mean()
 stock_data['sma_200'] = stock_data['Close'].rolling(window=200).mean()
 stock_data['signal'] = 0
+
 buy_signals = (stock_data['sma_50'] > stock_data['sma_200']) & (stock_data['sma_50'].shift(1) <= stock_data['sma_200'].shift(1))
 sell_signals = (stock_data['sma_50'] < stock_data['sma_200']) & (stock_data['sma_50'].shift(1) >= stock_data['sma_200'].shift(1))
 stock_data.loc[buy_signals, 'signal'] = 1
@@ -23,8 +24,8 @@ stock_data.loc[sell_signals, 'signal'] = -1
 
 latest_row = stock_data.iloc[[-1]]
 
-if latest_row['signal'].item() == 1:
-    order = api.submit_order(symbol=ticker_symbol, qty=10, side='buy', type='market', time_in_force='day')
+if latest_row['signal'].item() == 0:
+    order = api.place_order(ticker_symbol, 10, "buy")
     print("Trade executed:", order)
 else:
     print("No buy signal on the latest date.")
